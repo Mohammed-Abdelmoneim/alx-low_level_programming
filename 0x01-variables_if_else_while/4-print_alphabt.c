018a42ae2431dc83889f691f97807d4e8100b25c
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-11 09:34:33 -0700

    w

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet except q and c
+ *
+ * Return: 0 with print fuction
+ */
+int main(void)
+{
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+	{
+		if (l != 'q' || l != 'e')
+			putchar(l);
+	}
+
+	putchar('\n');
+	return (0);
+}
+
