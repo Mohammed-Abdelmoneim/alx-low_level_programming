dd3f87cd915d2fd3519387ae25518a65ba74a32d
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-11 14:23:32 -0700

    #

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+* main - prints the alphabet
+*
+* Return: 0 with print fuction
+*/
+int main(void)
+{
+
+	int i;
+	int l;
+	int res;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+
+		for (l = '0'; l <= '9'; l++)
+		{
+			if (i != l && (i < l))
+			{
+				putchar(i);
+				putchar(l);
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
