4e43c1fe1893743562419004c6c788327c883066
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-11 10:50:24 -0700

    #

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - prints the alphabet
+ *
+ * Return: 0 with print fuction
+ */
+int main(void)
+{
+	int l;
+
+	for (l = '0'; l >= '9'; l--)
+	{
+		putchar(l);
+		if (l != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
+
