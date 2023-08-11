6c63810037335142cc10c3f5da22e5492b4f8763
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-11 15:13:42 -0700

    w

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,35 @@
+/**
+* main - prints all possible different combinations of three digits.
+*
+* Return: 0 and print function
+*/
+int main(void)
+{
+	int i;
+	int l;
+	int t;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		for (l = '0'; l <= '9'; l++)
+		{
+			for (t = '0'; t <= '9'; t++)
+			{
+				if ((i != l && l != t && i != t) && (l < t) && (i < l))
+				{
+					putchar(i);
+					putchar(l);
+					putchar(t);
+					if (i != '7' || l != '8' || t != '9')
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
+
