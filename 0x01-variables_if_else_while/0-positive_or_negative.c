3a24d638f63305100439c67edd6ac3240c339d2e
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-11 07:40:30 -0700

    new dir

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+* main - print random number with string descriping
+* the value of the number
+*
+* Return: if n is positive return is positive
+*if n is equal zero return is zero
+*if n is positive return is negative
+*/
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+		printf("%d is positive", n);
+	else if (n == 0)
+		printf("%d is zero", n);
+	else
+		printf("%d is negative", n);
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/README.md
@@ -0,0 +1,1 @@
+Alx variables and loops repo
