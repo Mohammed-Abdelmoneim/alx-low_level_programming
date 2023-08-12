e7cea802338da93698fc1d1d9acf63cd87318f83
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-11 19:34:11 -0700

    w

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,17 +7,21 @@
 int main(void)
 {
 	int i, j;
-	
+
 	for (i = 0; i <= 99; i++)
 	{
-		for (j = i + 1; j <= 99; j++) {
+		for (j = i + 1; j <= 99; j++)
+		{
 			putchar('0' + i / 10);
 			putchar('0' + i % 10);
 			putchar(' ');
 			putchar('0' + j / 10);
 			putchar('0' + j % 10);
-			putchar(',');
-			putchar(' ');
+			if (i != 99; || j != 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
