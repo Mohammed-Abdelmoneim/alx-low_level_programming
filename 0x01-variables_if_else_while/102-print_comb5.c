1002c5c7590e3df3fa77a84c87b2d28633accc78
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-11 19:42:39 -0700

    3

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,18 +8,18 @@ int main(void)
 {
 	int i, j;
 
-	for (i = 0; i <= 99; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (j = 0; j <= 99; j++)
+		for (j = 0;i j < 100; j++)
 		{
 			if (i < j)
 			{
-				putchar(( i / 10) + 48);
-				putchar(( i % 10) + 48);
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
-				putchar(( j / 10) + 48);
-				putchar(( j % 10) + 48 );
-				if (i != 99 || j != 99)
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
 				{
 					putchar(',');
 					putchar(' ');
