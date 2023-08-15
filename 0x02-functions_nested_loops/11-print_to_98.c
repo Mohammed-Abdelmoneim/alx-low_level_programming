d1f834917e158cfcc292282f1c7a2b46a866a7d8
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-15 13:45:00 -0700

    a

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ *  print_to_98 - print nums from 0 to 98
+ * @n: first operand
+ *
+ * Return: the value of addition
+ */
+
+int add(int n)
+{
+	int r = n;
+
+	while (r <= 98)
+	{
+		printf("%d, ", r);
+		r++;
+	}
+	while (r >= 98)
+	{
+		printf("%d, ", r);
+		r--;
+	}
+
+	return (r);
+}
