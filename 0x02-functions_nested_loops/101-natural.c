35fbaed47fb96665ef4f9d876b33197f1e8c341f
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-15 14:44:52 -0700

    a

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Find the sum of all the multiples of 3 or 5 below 1000
+ *
+ * Return: value of sum
+ */
+int main(void)
+{
+	int n = 0;
+	int sum = 0;
+
+	for (int n = 3; n < 1024; n++)
+	{
+		if (n % 3 == 0 || n % 5 == 0)
+			sum += i;
+	}
+	return (sum)
+}
