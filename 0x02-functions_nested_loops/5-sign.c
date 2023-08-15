f9c67b3799d47f07d20839acdc15a97ffa175338
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-15 04:22:50 -0700

    A:

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,22 +11,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
 		_putchar('1');
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		_putchar(',');
-		_putchar(' ');
 		_putchar('0');
 	}
 	else
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
 		_putchar(n + '0');
 	}
 	return (0);
