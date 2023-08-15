92733696acb94b3fedd0285fa0ec396fd5b90218
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-15 14:54:08 -0700

    a

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -8,15 +8,14 @@ int main(void)
 {
 	int n;
 	int sum = 0;
-	
+
 	for (n = 3; n < 1024; n++)
 	{
 		if (n % 3 == 0 || n % 5 == 0)
 		{
-		
 			sum += n;
-			printf("%d\n", sum);
 		}
 	}
+	printf("%d", sum);
 	return (sum);
 }
