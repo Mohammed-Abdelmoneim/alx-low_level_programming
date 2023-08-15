87b1ff897dfcfb29640c459bfa79addcfee3330c
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-15 02:04:19 -0700

    a

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * main - prints _putchar
+ *
+ * Return: 1 if true
+ * 0 if false
+ */
+int main(void)
+{
+	int i = 0;
+	char s[8] = "_putchar";
+
+	while (i < 8)
+	{
+		putchar(s[i]);
+		i++;
+	}
+	return (0);
+}
