54ef1ac052394d4a8aca1a8e96a661f4baa7bf4a
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-15 02:53:26 -0700

    w

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print alphabet 10 times
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	int lines = 0;
+
+	while (lines < 10)
+	{
+		i = 97;
+		while (i <= 122)
+		{
+			_putchar(i);
+			i++;
+		}
+		putchar('\n');
+		lines++;
+	}
+}
+
