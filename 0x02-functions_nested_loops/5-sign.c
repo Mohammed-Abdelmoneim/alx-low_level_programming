824473e64a0ba82b77eb13a31f30514dda744b22
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-15 04:20:25 -0700

    a

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_sign - prints the sign of the number
+ * @n: first operand
+ *
+ * Return: 1 if number greater than 0, 0 if = 0, -1 if less then 0
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		_putchar(',');
+		_putchar(' ');
+		_putchar('1');
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar('0');
+	}
+	else
+	{
+		_putchar('-');
+		_putchar(',');
+		_putchar(' ');
+		_putchar(n + '0');
+	}
+
+}
+
