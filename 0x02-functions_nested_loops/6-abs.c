afb5015d137b062598e6a25d37ed6bd68f4a1e80
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-15 04:37:15 -0700

    A" "

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdlib.ih>
+/**
+ * _isabs - prints 1 if c is lowercase else prints 0
+ *@int: first operand
+ *
+ * Return: 1 if c is lowercar 0 if else
+ */
+int _isabs(int)
+{
+	int r = abs(int);
+
+	return (r);
+}
