9d21a237f0315c861bc7e775330143ae4cf44b97
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-17 13:34:37 -0700

    a

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -9,17 +9,17 @@ void print_line(int n)
 {
 	int i;
 
-	while (i < n)
+	if (n <= 0)
 	{
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
-		else
+		_putchar('\n');
+	}
+	else
+	{
+		while (i < n)
 		{
 			_putchar('_');
+			i++;
 		}
-		i++;
 	}
 	_putchar('\n');
 }
