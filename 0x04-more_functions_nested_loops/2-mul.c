a2dbb25454b6cae808f2c07b6dd59f6fbf88382e
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-17 09:23:20 -0700

    Ag

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * mul - multiplies two integers
+ * @a: first param
+ * @b: second param
+ *
+ * Return: 0
+ */
+int  mul(int a, int b)
+{
+	return (a * b);
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,5 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 #include <stdio.h>
 #include <ctype.h>
 
 int _isupper(int c);
 int _isdigit(int c);
+int mul(int a, int b);
+
+#endif /* MAIN_H */
