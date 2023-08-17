9dda8eaca1aa253a01b736b12a7dba816ed47cb2
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-17 12:08:25 -0700

    a

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_most_numbers - prints numbers from 0 to 9
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	while (i <= 9)
+	{
+		j = 0;
+
+		while (j <= 14)
+		{
+			if(j < 10)
+			{
+				_putchar((j % 10) + '0');
+			}
+			else if (j >= 10 && j != 0)
+			{
+				_putchar((j / 10) + '0');
+				_putchar((j % 10) + '0');
+			}
+			j++;
+		}
+		_putchar('\n')
+		i++;
+	}
+	_putchar('\n');
+}
