7e31453ed1bf5a519a8fab66a4bf54fbb139978d
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-17 05:07:32 -0700

    a

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -21,7 +21,7 @@ void print_remaining_days(int month, int day, int year)
 		else if (month == 2 && day == 29)
 		{
 			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 366 - day);	
+			printf("Remaining days: %d\n", 366 - day);
 		}
 
 		printf("Day of the year: %d\n", day);
