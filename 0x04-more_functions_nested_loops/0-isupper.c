2252ceee30b787be937aae5a4dbba6438220de50
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-17 09:15:19 -0700

    A

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,6 +1,12 @@
 #include "main.h"
 #include <ctype.h>
 
+/**
+ * _isupper - prints 1 if letter is uppercase, 0 otherwise
+ * @c: first param
+ *
+ * Return: 0
+ * */
 int  _isupper(int c)
 {
 	if (isupper(c) != 0)
