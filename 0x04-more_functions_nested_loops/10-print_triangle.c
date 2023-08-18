0a24d00d36c5ba8025247026895b5ee78482cbab
mohammed abdelmoneim <mohamedabdelmoneim95@gmail.com>
2023-08-17 17:41:48 -0700

    1

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - print _ based on n number
+ * @size: first param
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = 1; j <= size - i; j++)
+			{
+				putchar(' ');
+			}
+			for (j = 1; j <= i; j++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+}
