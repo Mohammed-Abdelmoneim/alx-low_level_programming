9678b912cf36443256462366afdf8da8f1ca1970
root <root@Moneim-PC.localdomain>
2023-08-21 19:23:11 +0300

    A

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include "string.h"
+/**
+ * _strlen - prints the length of a string
+ * @s: first param
+ *
+ * Return: 0 success
+ */
+int _strlen(char *s)
+{
+	int r = strlen(s);
+
+	return (r);
+}
+
