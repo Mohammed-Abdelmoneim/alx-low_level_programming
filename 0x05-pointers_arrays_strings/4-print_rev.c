91c0847a56a70b2eb3c8bf53ce8ecfa02cddf552
root <root@Moneim-PC.localdomain>
2023-08-21 19:37:18 +0300

    A

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_rev - prints a string, in reverse
+ * @str: first param
+ *
+ * Return: 0 success
+ */
+void print_rev(char *str)
+{
+	printf(strrev(str));
+}
+
