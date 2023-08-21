e54718092634ed21a5a62cadacb395556c8383b4
root <root@Moneim-PC.localdomain>
2023-08-21 23:26:50 +0300

    Ag

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -14,8 +14,7 @@ void puts_half(char *str)
 
 	if (len % 2 != 0)
 	{
-		halflen = (len - 1) / 2;
-		for (i = halflen; i < len + 1; i++)
+		for (i = halflen; i < len - 1; i++)
 		{
 			_putchar(*(str + i));
 		}
