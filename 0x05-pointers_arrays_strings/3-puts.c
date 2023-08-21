b976f1b71198fa79e5b34048c9ad389cc78b370b
root <root@Moneim-PC.localdomain>
2023-08-21 19:28:23 +0300

    A

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include "string.h"
+/**
+ * _puts - Write a function that prints a string
+ * @str: first param
+ *
+ * Return: 0 success
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
+
