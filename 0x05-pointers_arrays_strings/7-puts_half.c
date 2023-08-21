cee2a45e0b5dce2fbd76241b25838465404ceeff
root <root@Moneim-PC.localdomain>
2023-08-21 22:53:57 +0300

    A

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts_half -  prints half of a string
+ * @str: first param
+ *
+ * Return: 0 success
+ */
+void puts_half(char *str)
+{
+	int i;
+	int len = strlen(str);
+	int halflen = len / 2;
+
+	for (i = halflen; i > len; i++)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
+
