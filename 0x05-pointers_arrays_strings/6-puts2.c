9394a407c6a26090510922df493a4648a493142c
root <root@Moneim-PC.localdomain>
2023-08-21 21:53:05 +0300

    Ag

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <string.h>
+/**
+ * puts2 - prints a string,
+ * @str: first param
+ *
+ * Return: 0 success
+ */
+void puts2(char *str)
+{
+	int i;
+	int l = strlen(str);
+
+	for (i = 0; i <= l; i = i + 1)
+	{
+		_putchar(*(str + i));
+	}
+	_putchar('\n');
+}
+
