0cfe801ae54c06db8354656e90073d4cb2815815
root <root@Moneim-PC.localdomain>
2023-08-24 02:11:12 +0300

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -10,12 +10,17 @@ void reverse_array(int *a, int n)
 {
 	int i;
 
-	for (i = n - 1; i >= 0; i--)
+	if (n <= 0)
 	{
-		if (i != 0)
-			printf("%d, ", a[i]);
-		else
-			printf("%d ", a[i]);
+		printf("\n");
 	}
-	printf("\n");
+	else
+	{
+		for (i = n - 1; i > 0; i--)
+		{
+			printf("%d, ", a[i]);
+		}
+		printf("%d\n", a[0]);
+	}
 }
+
