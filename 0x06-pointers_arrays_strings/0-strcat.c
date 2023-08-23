25494892a27caa61343fe55f3ed46a83352f2658
root <root@Moneim-PC.localdomain>
2023-08-23 15:04:34 +0300

    0-strcat.c

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - concatenates two strings
+ * @src: the concat src
+ * @dest: the concat dest 
+ *
+ * Return: the concat result
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
+
