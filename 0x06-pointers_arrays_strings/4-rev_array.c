ec59231d324ada16f06dbd94f00f548587902808
root <root@Moneim-PC.localdomain>
2023-08-24 01:52:14 +0300

    4-rev_array.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,9 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 #include <string.h>
+/**
+ * reverse_array - reverses an array or numbers
+ * @a: first param
+ * @n: second param
+ */
 void reverse_array(int *a, int n)
 {
 	int i;
+
 	if (n <= 0)
 	{
 		printf("\n");
@@ -12,11 +18,10 @@ void reverse_array(int *a, int n)
 	{
 		for (i = n - 1; i >= 0; i--)
 		{
-		    if (i != 0)
+			if (i != 0)
 				printf("%d, ", *(a + i));
 			else
 				printf("%d", *(a + i));
-			
 		}
 		printf("\n");
 	}
