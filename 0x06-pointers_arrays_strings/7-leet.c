b6e4bcb3e804bd527131383484dc2c5bfff527f3
root <root@Moneim-PC.localdomain>
2023-08-24 06:09:53 +0300

    7-1337

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -17,27 +17,22 @@ char *leet(char *s)
 		while (s[i] == 'a' || s[i] == 'A')
 		{
 			s[i] = '4';
-			i++;
 		}
 		while (s[i] == 'e' || s[i] == 'E')
 		{
 			s[i] = '3';
-			i++;
 		}
 		while (s[i] == 'o' || s[i] == 'O')
 		{
 			s[i] = '0';
-			i++;
 		}
 		while (s[i] == 't' || s[i] == 'T')
 		{
 			s[i] = '7';
-			i++;
 		}
 		while (s[i] == 'l' || s[i] == 'L')
 		{
 			s[i] = '1';
-			i++;
 		}
 	}
 	return (s);
