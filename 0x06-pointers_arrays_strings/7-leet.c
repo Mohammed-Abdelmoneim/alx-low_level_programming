030d7be8660cbba334cef76e1354d2071c001e8d
root <root@Moneim-PC.localdomain>
2023-08-25 02:28:54 +0300

    7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,26 +1,25 @@
 #include "main.h"
 /**
  * leet - concatenates two strings
- * @s: the string
+ * @str: the string
  *
  * Return: the leet result
  */
-char *leet(char *s)
+char *leet(char *str)
 {
-	int i;
+	int i, j;
+	char *s = "aAeEoOtTlL";
+	char *d = "4433007711";
 
-	for (i = 0; s[i] != '\0' ; i++)
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == s[j])
+			{
+				str[i] = d[j];
+			}
+		}
 	}
-	return (s);
+	return (str);
 }
