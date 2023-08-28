d63c884f65baa6327e376612df8c1b80e37268a0
root <root@Moneim-PC.localdomain>
2023-08-28 15:49:38 +0300

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _memset - a function that fills memory with a constant byte
+ * @s: a pointer to the block of memory to fill
+ * @b: the value to be set
+ * @n: the number of bytes to be set to the value.
+ * 
+ * Return: 0 success
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	memset(s, b, n);
+	return (0);)
+}
+
