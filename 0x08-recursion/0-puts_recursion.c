559eafc7476ea77947cfcb0ea07629855f8dd47a
root <root@Moneim-PC.localdomain>
2023-08-31 00:18:27 +0300

    0-puts_recursion.c

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _puts_recursion - prints a string, followed by a new line.
+ * @s: first param
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+}
+
