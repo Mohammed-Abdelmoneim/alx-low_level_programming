8628796d7e0efd2912add622101f816ad92ad10d
root <root@Moneim-PC.localdomain>
2023-08-31 01:27:35 +0300

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <math.h>
+/**
+ * _pow_recursion - returns the value of x raised to the power of y.
+ * @x: base value
+ * @y: power value
+ *
+ * Return: 0 success
+ */
+int _pow_recursion(int x, int y)
+{
+	return (pow(x, y));
+}
+
