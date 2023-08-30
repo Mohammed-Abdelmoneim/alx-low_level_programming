4cdb8cdccfb86fc29b1338cbc5cd9c497237704a
root <root@Moneim-PC.localdomain>
2023-08-31 01:08:25 +0300

    2-strlen_recursion.c

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen_recursion - returns the length of a string.
+ * @s: first param
+ *
+ * Return: 0 success
+ */
+int _strlen_recursion(char *s)
+{
+	return (strlen(s));
+}
+
