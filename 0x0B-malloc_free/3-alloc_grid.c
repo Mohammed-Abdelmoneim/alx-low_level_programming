f35813f627374dcd85b9fb5fde2c78efd8723ad4
root <root@Moneim-PC.localdomain>
2023-09-05 20:24:04 +0300

    2-str_concat.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -3,32 +3,52 @@
 #include <stdio.h>
 
 /**
- * str_concat -  concatenates two strings.
- *@s1: first string
- *@s2: second string
+ * alloc_grid - returns a pointer to a 2 dimensional array of integers.
+ *@width: width of an array
+ *@height: height of an array
  *
  * Return: 0 success
  */
 int **alloc_grid(int width, int height)
 {
-  int *grid;
-  int i,j;
-  if (width == 0 || height == 0)
-  {
-    return NULL;
-  }
-  grid = malloc(sizeof(int) * (width * height));
+int **grid, i, j;
+if (width <= 0 || height <= 0)
+{
+return (NULL);
+}
+grid = (int **)malloc(sizeof(int) * height);
+
+for (i = 0; i < height; i++)
+{
+grid[i] = (int *)malloc(sizeof(int) * width);
+if (grid[i] == NULL)
+{
+for (j = 0; j < i; j++)
+{
+free(grid[j]);
+}
+free(grid);
+return (NULL);
+}
+}
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+{
+grid[i][j] = 0;
+}
+}
+for (i = 0; i < height; i++)
+{
+for (j = 0; j < width; j++)
+{
+printf("%d ", grid[i][j]);
+}
+printf("\n");
+}
 if (grid == NULL)
 {
 return (NULL);
 }
-  for (i = 0; i < width; i++)
-  {
-    for (j = 0; j < height; j++)
-    {
-      printf("%d ", grid[i]);
-    }
-    printf("\n");
-  }
-  return (0);
+return (grid);
 }
