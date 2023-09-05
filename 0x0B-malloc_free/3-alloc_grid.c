6f838bf448ac918d7a2895354dc2182789077b31
root <root@Moneim-PC.localdomain>
2023-09-05 20:34:21 +0300

    3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -17,6 +17,10 @@ if (width <= 0 || height <= 0)
 return (NULL);
 }
 grid = (int **)malloc(sizeof(int) * height);
+if (grid == NULL)
+{
+return (NULL);
+}
 
 for (i = 0; i < height; i++)
 {
@@ -37,11 +41,6 @@ for (j = 0; j < width; j++)
 {
 grid[i][j] = 0;
 }
-}
-
-if (grid == NULL)
-{
-return (NULL);
 }
 return (grid);
 }
