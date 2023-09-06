1e9d4c11538a68a18f62c12cd74c449e10edd281
root <root@Moneim-PC.localdomain>
2023-09-07 02:31:38 +0300

    100-argstostr.c

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * argstostr - concatenates all the arguments of your program.
+ *@ac: number of args
+ *@s2: args string
+ *
+ * Return: 0 success
+ */
+char *argstostr(int ac, char **av)
+{
+  char *s;
+  int i;
+  /* if (ac == NULL || av == NULL)
+  {
+    return (NULL);
+  } */
+  s = (char *)malloc(ac * sizeof(char));
+  strcpy(s, av[1]);
+    strcat(s, "\n");
+  for (i = 2; i < ac; i++)
+  {
+    strcat(s, av[i]);
+    strcat(s, "\n");
+  }
+  return (s);
+}
