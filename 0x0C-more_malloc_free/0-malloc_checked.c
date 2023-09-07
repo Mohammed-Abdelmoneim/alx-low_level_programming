0530190a9e14989598d0d818ba31d21453d3f0b9
root <root@Moneim-PC.localdomain>
2023-09-08 01:11:15 +0300

    0-malloc_checked.c

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * malloc_checked - allocates memory using malloc.
+ * @b: number of memory
+*/
+void *malloc_checked(unsigned int b)
+{
+  int *ptr = malloc(b);
+  
+    exit(98);
+  
+  return(ptr);
+}
