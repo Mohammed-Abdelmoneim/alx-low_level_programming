27df1f00a526711742cdadbe3e7214bf0d264d5a
root <root@Moneim-PC.localdomain>
2023-09-07 03:09:45 +0300

    100-argstostr.c fix error

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -4,38 +4,36 @@
 /**
  * argstostr - concatenates all the arguments of your program.
  *@ac: number of args
- *@s2: args string
+ *@av: args string
  *
  * Return: 0 success
  */
 char *argstostr(int ac, char **av)
 {
-  int total_len = 0;
-  char *s;
-  int offset = 0; 
-  int i;
-  
-  if (ac == 0 || av == NULL)
-  {
-    return NULL;
-  }
+int total_len = 0;
+char *s;
+int offset = 0;
+int i;
 
-  for (i = 0; i < ac; i++)
-  {
-    total_len += strlen(av[i]) + 1; 
-  }
+if (ac == 0 || av == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < ac; i++)
+{
+total_len += strlen(av[i]) + 1;
+}
+s = (char *)malloc(total_len + 1);
 
-  s = (char *)malloc(total_len + 1);
-  if (s == NULL)
-  {
-    return NULL; 
-  }
-  for (i = 0; i < ac; i++)
-  {
-    strcpy(s + offset, av[i]);
-    offset += strlen(av[i]);
-    s[offset++] = '\n'; 
-  }
-
-  return s;
+if (s == NULL)
+{
+return (NULL);
+}
+for (i = 0; i < ac; i++)
+{
+strcpy(s + offset, av[i]);
+offset += strlen(av[i]);
+s[offset++] = '\n';
+}
+return (s);
 }
