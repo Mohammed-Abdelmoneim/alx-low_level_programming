0a07eb237e81064bbfbac1dd651b9ff62afee6df
root <root@Moneim-PC.localdomain>
2023-09-08 01:15:40 +0300

    0-malloc_checked.c

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -2,13 +2,15 @@
 /**
  * malloc_checked - allocates memory using malloc.
  * @b: number of memory
+ *
+ * Return: 0 succuss / 98 on fail
 */
 void *malloc_checked(unsigned int b)
 {
-  int *ptr = malloc(b);
-  if (ptr == NULL)
-  {
-    exit(98);
-  }
-  return(ptr);
+int *ptr = malloc(b);
+if (ptr == NULL)
+{
+exit(98);
+}
+return (ptr);
 }
