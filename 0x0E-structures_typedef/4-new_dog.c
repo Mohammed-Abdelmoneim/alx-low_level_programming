ff0d1ebe5ed9b6c88e7a628005c460ff57076e22
root <root@Moneim-PC.localdomain>
2023-09-11 13:56:59 +0300

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -20,8 +20,15 @@ dog_t *new_dog(char *name, float age, char *owner)
 	newDog->name = (char *)malloc(sizeof(char) * (strlen(name) + 1));
 	newDog->owner = (char *)malloc(sizeof(char) * (strlen(owner) + 1));
 
-	if (newDog->name == NULL || newDog->owner == NULL)
+	if (newDog->name == NULL)
 	{
+		free(newDog);
+		return (NULL);
+	}
+	if (newDog->owner == NULL)
+	{
+		free(newDog);
+		free(newDog->name);
 		return (NULL);
 	}
 	strcpy(newDog->name, name);
