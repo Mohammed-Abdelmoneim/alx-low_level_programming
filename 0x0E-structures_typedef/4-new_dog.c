211b60e9c9360df0fb23e31d52d29a581641d73c
root <root@Moneim-PC.localdomain>
2023-09-11 13:12:29 +0300

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,25 +2,30 @@
 #include <stdlib.h>
 #include "dog.h"
 /**
- * print_dog - prints a struct dog
- * @d: struct
+ * new_dog - prints a struct dog
+ * @name: his name
+ * @age: his age
+ * @owner: his owner
+ *
+ * Return: 0 successs
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newDog = malloc(sizeof(dog_t));
 
+	if (newDog == NULL)
+		return (NULL);
+
 	char *nname = name;
 	char *nowner = owner;
 	float nage = age;
+
 	if (nage == 0)
 		age = 0;
 	if (nname == NULL)
 		name = "(nil)";
 	if (nowner == NULL)
 		owner = "(nil)";
-	if (newDog == NULL)
-		return (NULL);
 
 	return (newDog);
 }
-
