aae0d49381c7309b548498f9ea51d0939bc6c464
root <root@Moneim-PC.localdomain>
2023-09-14 00:09:40 +0300

    2-int_index.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,28 @@
+#include "function_pointers.h"
+#include <stdio.h>
+/**
+ * int_index - a function that searches for an integer.
+ * @array: the array given
+ * @size: the size of the array
+ * @cmp: the function to be used to compare values
+ *
+ * Return: 0 success
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i, res;
+
+	if (!array || !cmp)
+		return (0);
+	if (size <= 0)
+		return (0);
+
+	for (i = 0; i < size; i++)
+	{
+		res = (*cmp)(array[i]);
+		if (res != 0)
+			return (i);
+	}
+	return (-1);
+	return (i);
+}
