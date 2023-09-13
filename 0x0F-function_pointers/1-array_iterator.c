cfddd687af9012f6a2dff1183e0e11235a9a0cf9
root <root@Moneim-PC.localdomain>
2023-09-13 23:27:17 +0300

    1-array_iterator.c

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - prints name!
+ * @array: array given
+ * @size: size of the array
+ * @action: a pointer to the function you need to use
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+	{
+		action(array[i]);
+	}
+}
