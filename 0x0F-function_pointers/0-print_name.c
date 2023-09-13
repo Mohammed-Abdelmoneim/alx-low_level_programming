7f5591520a0b6cb4b725dc4f4b921ecdc8a793f6
root <root@Moneim-PC.localdomain>
2023-09-13 22:59:07 +0300

    0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,10 @@
+#include "function_pointers.h"
+/**
+ * print_name - prints name!
+ * @name: the name
+ * @f: the print function
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	(*f)(name);
+}
