24a305c0d70efd03832c5ceb8e1459900a18fc88
root <root@Moneim-PC.localdomain>
2023-09-13 23:10:16 +0300

    0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,5 @@
 #include "function_pointers.h"
+#include <stdlib.h>
 /**
  * print_name - prints name!
  * @name: the name
@@ -6,5 +7,7 @@
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (name == NULL)
+		exit(EXIT_FAILURE);
 	(*f)(name);
 }
