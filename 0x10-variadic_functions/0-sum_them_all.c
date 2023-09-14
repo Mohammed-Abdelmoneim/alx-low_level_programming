73557a8843a04c7668073a4420cd91e706d061d6
root <root@Moneim-PC.localdomain>
2023-09-14 22:42:56 +0300

    0-sum_them_all.c

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -2,21 +2,23 @@
 /**
  * sum_them_all - returns the sum of all its parameters
  * @n: number of params
- * 
+ *
  * Return: the sum of all params
 */
 int sum_them_all(const unsigned int n, ...)
 {
 	int res = 0;
 	unsigned int i = 0;
+
 	va_list args;
+
 	va_start(args, n);
-	
+
 	if (n == 0)
 	{
-		return (0); 
+		return (0);
 	}
-	
+
 	for (i = 0; i < n; i++)
 	{
 		res += va_arg(args, int);
