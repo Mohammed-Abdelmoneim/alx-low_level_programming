2eac87e31a7427ee1e25ebf3eaf55096c6ba13e9
root <root@Moneim-PC.localdomain>
2023-09-15 01:45:36 +0300

    3-print_all.c

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,47 @@
+#include "variadic_functions.h"
+/**
+ * print_all -  prints numbers, followed by a new line.
+ * @format: list of types of arguments passed to the function
+*/
+void print_all(const char * const format, ...)
+{
+	int i = 0;
+	char *str;
+	char *s = "";
+
+	va_list args;
+
+	va_start(args, format);
+
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+			case 'c':
+				printf("%s%c", s, va_arg(args, int));
+				break;
+			case 'i':
+				printf("%s%i", s, va_arg(args, int));
+				break;
+			case 'f':
+				printf("%s%f", s, va_arg(args, double));
+				break;
+			case 's':
+				str = va_arg(args, char*);
+				if (!str)
+					printf("(nil)");
+				printf("%s%s", s, str);
+				break;
+			default:
+				i++;
+				continue;
+			}
+			s = ", ";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
