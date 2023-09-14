a4a09c680b7f8184154428edbce175bb302768f6
root <root@Moneim-PC.localdomain>
2023-09-14 04:02:06 +0300

    3-main.c

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -8,12 +8,12 @@
  * struct op - Struct op
  *
  * @op: The operator
- * @f: The function associated
+ * @f: The function
  */
 typedef struct op
 {
-    char *op;
-    int (*f)(int a, int b);
+		char *op;
+		int (*f)(int a, int b);
 } op_t;
 
 int (*get_op_func(char *s))(int, int);
