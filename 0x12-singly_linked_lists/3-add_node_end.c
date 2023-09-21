8dadf70d1ff5be4f3a2d9a75eded6daa4d1bff21
root <root@Moneim-PC.localdomain>
2023-09-22 00:54:17 +0300

    3-add_node_end.c

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -2,38 +2,38 @@
 
 
 /**
- * print_list - that prints all the elements of a list_t list
- * a linked list_t list
- * @h: linked list
+ * add_node_end - adds a new node at the end of a list_t list.
+ * @head: head
+ * @str: string to be add
  *
  * Return: 0 success
 */
 list_t *add_node_end(list_t **head, const char *str)
 {
-  list_t *new;
-  list_t *temp;
+	list_t *new;
+	list_t *temp;
 
-  if(!head)
-    return (NULL);
-  
-  new = malloc(sizeof(list_t));
-  if (!new)
-    return (NULL);
-  
-  new->str = strdup(str);
-  new->len = strlen(str);
-  new->next = NULL;
+	if (!head)
+		return (NULL);
 
-  if (!*head)
-  {
-    *head = new;
-    return (new);
-  }
-  temp = *head;
+	new = malloc(sizeof(list_t));
+	if (!new)
+		return (NULL);
 
-  while (temp->next)
-    temp = temp->next;
-  temp->next = new;
-  
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = NULL;
+
+	if (!*head)
+	{
+		*head = new;
+		return (new);
+	}
+	temp = *head;
+
+	while (temp->next)
+		temp = temp->next;
+	temp->next = new;
+
 	return (new);
 }
