a1d15a2518b5bb5be1e3943e8f74d33d6c7bdaa9
root <root@Moneim-PC.localdomain>
2023-09-21 23:58:01 +0300

    0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,31 @@
+#include "lists.h"
+
+
+/**
+ * print_list - that prints all the elements of a list_t list
+ * a linked list_t list
+ * @h: linked list
+ *
+ * Return: 0 success
+*/
+size_t print_list(const list_t *h)
+{
+	int count = 0;
+
+	const list_t *ptr = NULL;
+
+	ptr = h;
+
+	if (ptr->str == NULL)
+	{
+		printf("[0] (nil)\n");
+	}
+
+	while (ptr != NULL)
+	{
+		count++;
+		printf("[%d] %s\n", ptr->len, ptr->str);
+		ptr = ptr->next;
+	}
+	return (count);
+}
