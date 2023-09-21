178344495695ec8768375e5995c91140b3d3f689
root <root@Moneim-PC.localdomain>
2023-09-22 00:59:44 +0300

    2-add_node.c

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+
+/**
+ * add_node - adds a new node at the end of a list_t list.
+ * @head: head
+ * @str: string to be add
+ *
+ * Return: the address of the new element, or NULL if it failed
+*/
+list_t *add_node(list_t **head, const char *str)
+{
+ist_t *temp;
+
+	temp = malloc(sizeof(list_t));
+
+	if (temp == NULL)
+		return (NULL);
+
+	temp->str = strdup(str);
+
+	if (temp->str == NULL)
+		return (NULL);
+
+	temp->len = strlen(str);
+	temp->next = *head;
+	*head = temp;
+
+	return (temp);
+}
