44118d912ec1e9f389aa971492e22cbe0531842e
root <root@Moneim-PC.localdomain>
2023-09-22 00:08:53 +0300

    0-print_list.c

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -12,19 +12,17 @@ size_t print_list(const list_t *h)
 {
 	int count = 0;
 
-	const list_t *ptr = NULL;
+	const list_t *ptr;
 
 	ptr = h;
-
-	if (ptr->str == NULL)
-	{
-		printf("[0] (nil)\n");
-	}
 
 	while (ptr != NULL)
 	{
 		count++;
-		printf("[%d] %s\n", ptr->len, ptr->str);
+		if (ptr->str == NULL)
+			printf("[0] (nil)\n");
+		else
+			printf("[%d] %s\n", ptr->len, ptr->str);
 		ptr = ptr->next;
 	}
 	return (count);
