c78111b858b3ac65f1c57e61050ae94ee02348a9
root <root@Moneim-PC.localdomain>
2023-11-25 00:40:45 +0200

    fix betty

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,26 +1,32 @@
 #include "main.h"
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: is pointing to a string of 0 and 1 chars
+ *
+ * Return: the nunber in decimal
+ */
 
 unsigned int binary_to_uint(const char *b)
 {
-  if (b == NULL)
-    return 0;
-  
-  unsigned int res = 0;
-  int i;
+	unsigned int res = 0;
+	int i;
 
-  for(i = 0; b[i] != '\0'; i++)
-  {
-    res = res << 1;
+	if (b == NULL)
+		return (0);
 
-    if (b[i] == '1')
-    {
-      res = res | 1;
-    }
-    else if (b[i] != '0')
-    {
-      return 0;
-    }
-  }
+	for (i = 0; b[i] != '\0'; i++)
+	{
+		res = res << 1;
 
-  return res;
+		if (b[i] == '1')
+		{
+			res = res | 1;
+		}
+		else if (b[i] != '0')
+		{
+			return (0);
+		}
+	}
+
+	return (res);
 }
